b9e9d59222e0132bb1047027021951a4183b0058
Matt Andrews <matt@mattandre.ws>
2013-03-07 17:49:52 +0000

    Putting back things I shouldn't have deleted.

--- _tests/buster.js
+++ _tests/buster.js
@@ -3,6 +3,12 @@ var config = module.exports;
 config["DelegateTests"] = {
 	rootPath: '../',
     environment: "browser",
+    extensions: [ require("buster-coverage") ],
+    "buster-coverage": {
+        outputDirectory: "build/logs/jscoverage",
+        format: "lcov",
+        combinedResultsOnly: true
+    },
     sources: [
         "lib/delegate.js"
     ],
