dc2cefc8700b353a5c7f2858692aafb03c6be0eb
Rhys Evans <rhys.evans@ft.com>
2014-05-19 11:54:34 +0100

    removed local copies of polyfills

--- test/buster.js
+++ test/buster.js
@@ -4,9 +4,12 @@ config.DelegateTests = {
   rootPath: '../',
   environment: "browser",
   sources: [
-    "test/polyfills/matches-selector.js",
-    "test/polyfills/event-listener.js",
-    "test/polyfills/es5-shim.js",
+
+    // The 3 polyfills below are needed for testing in ie8
+    // "https://gist.githubusercontent.com/jonathantneal/3062955/raw/ad9d969c4e55581edbbb293c74135a751f586664/matchesSelector.polyfill.js",
+    // "https://raw.githubusercontent.com/jonathantneal/EventListener/master/EventListener.js",
+    // "http://cdnjs.cloudflare.com/ajax/libs/es5-shim/2.3.0/es5-shim.min.js",
+    
     "build/dom-delegate.js"
   ],
   tests: [
